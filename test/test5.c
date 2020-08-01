2e3d0fc384da13885a651b28b34b2b3a8657efc0
pacovali <pacovali@student.codam.nl>
2020-08-01 14:22:55 +0200

    malloc

--- test/test5.c
+++ test/test5.c
@@ -8,11 +8,12 @@ void print(char *s)
 
 int main()
 {
-	malloc(1024);
-	malloc(1024 * 32);
-	malloc(1024 * 1024);
-	malloc(1024 * 1024 * 16);
-	malloc(1024 * 1024 * 128);
+	void *pointer;
+	pointer = malloc(1024);
+	pointer = malloc(1024 * 32);
+	pointer = malloc(1024 * 1024);
+	pointer = malloc(1024 * 1024 * 16);
+	pointer = malloc(1024 * 1024 * 128);
 	show_alloc_mem();
 	return(0);
 }
